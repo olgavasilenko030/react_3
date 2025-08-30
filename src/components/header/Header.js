98d471be399453506b66ecad72c6ef3990b5a4b9
Vasil_th18@top-academy.ru <Vasil_th18@top-academy.ru>
2025-08-30 22:29:21 +0500

    oop

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+import logo from'../../logo.svg';
+import React from 'react';
+import  './Header.css';
+class Header extends React.Component
+{
+    render()
+    {
+        return(
+        <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>STATE</h1>
         <a
@@ -16,8 +19,7 @@ function App() {
           Learn Git
         </a>
       </header>
-    </div>
-  );
+        )
+    }
 }
-
-export default App;
+export default Header;
